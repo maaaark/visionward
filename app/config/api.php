4bf62284d68615b03f4b163dee1aca50ff25ccba
klausbrandt <dualdeath@gmail.com>
2014-10-20 20:44:06 +0200

    include champion cronjob

---
+++ app/config/api.php
@@ -0,0 +1,8 @@
+<?php
+$setting_data['key'] = '9b856470-6702-4992-9217-56ac86b9170e';
+$setting_data['refresh_time'] = 5;
+$setting_data['use_riot_api'] = 1;
+$setting_data['problems'] = 0;
+
+
+return $setting_data;
