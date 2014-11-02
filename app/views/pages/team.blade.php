ffee30b828cb72fb4ad777f0b99fced89ce5f030
Matthias <matthias@lolquest.de>
2014-11-02 19:46:54 +0100

    Added ryanjermaine to Teamsite with image

--- app/views/pages/team.blade.php
+++ app/views/pages/team.blade.php
@@ -59,14 +59,19 @@
 	<br/><br/><br/>
 	<div class="row">
 		<div class="col-md-3">
-			<h2 class="headline">Matthias</h2>
+			<h2 class="headline">Matthias MacKenzie</h2>
 			<div style="text-align: center;">
-				<img src="/img/team/mark.jpg" class="img-circle" width="150" height="150" /><br/><br/>
+				<img src="/img/team/ryanjermaine.jpg" class="img-circle" width="150" height="150" /><br/><br/>
 			</div>
 			<strong>Summoner: </strong>ryanjermaine<br/>
 			<strong>Aufgabe: </strong>Code<br/>
 			<br/>
-			-
+			Teil des Teams wurde ich durch lolquest.net . Seitdem unterstütze ich Mark wo es nur geht. Meistens übernehm ich die Sachen die irgendwann mal anfallen :)
+			<br/>
+			<br/>
+			<div class="center">
+				<a href="https://twitter.com/ryanjermaine86" target="blank"><img src="/img/twitter.png" alt="@ryanjermaine86" width="35" /></a>
+			</div>
 		</div>
 	</div>
 @stop
