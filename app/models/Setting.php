42a60675bfc471b6832e4d2562877e90add9f4b3
Matthias <matthias@lolquest.de>
2014-11-06 22:40:12 +0100

    Commit Admin Settings

--- app/models/Setting.php
+++ app/models/Setting.php
@@ -1,5 +1,85 @@
 <?php
 
 class Setting extends \Eloquent {
-	protected $fillable = [];
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'settings';
+	protected $fillable = array('value');
+	
+	public static $rules = array(
+		'value'=>'required'
+	);
+	
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = array('password');
+
+	/**
+	 * Get the unique identifier for the user.
+	 *
+	 * @return mixed
+	 */
+	public function getAuthIdentifier()
+	{
+		return $this->getKey();
+	}
+
+	/**
+	 * Get the password for the user.
+	 *
+	 * @return string
+	 */
+	public function getAuthPassword()
+	{
+		return $this->password;
+	}
+
+	/**
+	 * Get the e-mail address where password reminders are sent.
+	 *
+	 * @return string
+	 */
+	public function getReminderEmail()
+	{
+		return $this->email;
+	}
+	
+	public function getRememberToken()
+	{
+		return $this->remember_token;
+	}
+
+	public function setRememberToken($value)
+	{
+		$this->remember_token = $value;
+	}
+
+	public function getRememberTokenName()
+	{
+		return 'remember_token';
+	}
+
+	public function roles()
+    {
+        return $this->belongsToMany('Role');
+    }
+	
+	public function hasRole($key)
+    {
+        foreach($this->roles as $role){
+            if($role->name === $key)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
