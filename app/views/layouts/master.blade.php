927d5dcfe536ec18c5af88d9d9d8e866cac4fbb4
Mark Tomicki <marktomicki@gmail.com>
2014-11-14 17:44:18 +0100

    gallery

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -35,13 +35,6 @@
 	<div class="container-fluid">
 	<div class="header_margin hidden-xs hidden-sm">
 		<div class="row">
-		  <!-- Indicators 
-		  <ol class="carousel-indicators">
-		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-		    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-		  </ol>-->
-		  
 		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 		  <!-- Wrapper for slides -->
 		  <div class="carousel-inner">
--- app/views/posts/gallery.blade.php
+++ app/views/posts/gallery.blade.php
@@ -1,29 +1,34 @@
-<h2 class="headline">Gallerie</h2>
-<div class="container-fluid header_margin">
-		<div class="row">
 
-		  
+<h2 class="headline">Gallerie</h2>
+
 		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 		  <!-- Wrapper for slides -->
 		  <div class="carousel-inner">
+		 <?php $i=0; ?>
+		 @foreach($post->gallery->pictures as $image)
+		  @if($i==0)
+		 	 <div class="item active">
+		 	 <?php $i++; ?>
+		 @else
+		 	 <div class="item">
+		 @endif
+				<div class="">
+					<img src="/uploads/galleries/{{ $image->destination }}/{{ $image->filename }}" width="100%" />
+				</div>
+		  </div>
+		  @endforeach
 		  
-		  <img src="/img/header/@yield('header_image', 'header.jpg')">
-		  <img src="/img/header/@yield('header_image', 'header.jpg')">
-		
-		  </div> <!-- end carousel inner -->
+		</div> <!-- end carousel inner -->
+  	  
+	  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+  	    <span class="glyphicon glyphicon-chevron-left"></span>
+  	  </a>
+  	  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+  	    <span class="glyphicon glyphicon-chevron-right"></span>
+  	  </a>
 	  
-	  <!-- Controls -->
-	  <a class="left carousel-control" href="#" role="button" data-slide="prev">
-	    <span class="glyphicon glyphicon-chevron-left"></span>
-	  </a>
-	  <a class="right carousel-control" href="#" role="button" data-slide="next">
-	    <span class="glyphicon glyphicon-chevron-right"></span>
-	  </a>
-	  
-		</div>
+	</div>		  
+
 		<div class="gallery_description">
 			{{ $post->gallery->description }}
-		</div>
-		  
-		</div>
-	</div>
+		</div>		  
