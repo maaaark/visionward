34e3fa16b0dcaa01c2dce973173833f093b19a23
Mark Tomicki <marktomicki@gmail.com>
2014-11-14 19:25:53 +0100

    summoner details

---
+++ app/views/layouts/summoner_sidebar.blade.php
@@ -0,0 +1,34 @@
+<h2 class="headline_no_border">Saison 4 Champions</h2>
+<table class="table table-striped">
+	<tr>
+		<td valign="top" width="65"><img src="http://ddragon.leagueoflegends.com/cdn/{{ $patchversion }}/img/champion/Blitzcrank.png" class="img-circle" width="50" /></td>
+		<td valign="top">
+			<strong>Blitzcrank</strong>&nbsp;&nbsp;&nbsp;<span class="kda"><span class="games_won">46</span>/<span class="games_loss">28</span> (75%)</span><br/>
+			<span class="kda">218/121/302 (4.6 KDA)</span>
+		</td>
+	</tr>
+</table>
+<br/>
+<h2 class="headline_no_border">Saison 4 Statistik</h2>
+<table class="table table-striped">
+	<tr>
+		<td valign="top" width="80"><strong>Stat</strong></td>
+		<td valign="top">Value</td>
+	</tr>
+	<tr>
+		<td valign="top" width="80"><strong>Stat</strong></td>
+		<td valign="top">Value</td>
+	</tr>
+	<tr>
+		<td valign="top" width="80"><strong>Stat</strong></td>
+		<td valign="top">Value</td>
+	</tr>
+	<tr>
+		<td valign="top" width="80"><strong>Stat</strong></td>
+		<td valign="top">Value</td>
+	</tr>
+	<tr>
+		<td valign="top" width="80"><strong>Stat</strong></td>
+		<td valign="top">Value</td>
+	</tr>
+</table>
