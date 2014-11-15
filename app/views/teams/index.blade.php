169041f666664c1f1329c01ec1ed27679f0f8a65
Matthias <matthias@lolquest.de>
2014-11-15 13:59:04 +0100

    Croppa implemented for sidebar, posts and teams

--- app/views/teams/index.blade.php
+++ app/views/teams/index.blade.php
@@ -7,51 +7,51 @@
 <table width="100%" class="team_overview">
 	<tr>
 		<td valign="top">
-			<h2><img src="/img/teams/euLCS.png" height="40" /> <span class="hidden-xs hidden-sm">EU LCS</span></h2>
+			<h2><img src="<?=Croppa::url('/img/teams/euLCS.png', 40, null)?>" height="40" /> <span class="hidden-xs hidden-sm">EU LCS</span></h2>
 			<table class="table table-striped">
 				@foreach($eulcs->teams as $team)
 				<tr>
-					<td><a href="/teams/{{ $team->id }}/{{ $team->slug }}"><img class="" src="/img/teams/logos/{{ $team->logo }}" width="18" />&nbsp;&nbsp;<img class="hidden-xs hidden-sm" src="/img/flags/{{ $team->country }}.png" />&nbsp;&nbsp;<span class="hidden-xs hidden-sm">{{ $team->name }}</span></a></td>
+					<td><a href="/teams/{{ $team->id }}/{{ $team->slug }}"><img class="" src="<?=Croppa::url('/img/teams/logos/'.$team->logo, 18, null)?>" width="18" />&nbsp;&nbsp;<img class="hidden-xs hidden-sm" src="/img/flags/{{ $team->country }}.png" />&nbsp;&nbsp;<span class="hidden-xs hidden-sm">{{ $team->name }}</span></a></td>
 				</tr>
 				@endforeach
 			</table>
 		</td>
 		<td valign="top">
-			<h2><img src="/img/teams/naLCS.png" height="40" /> <span class="hidden-xs hidden-sm">NA LCS</span></h2>
+			<h2><img src="<?=Croppa::url('/img/teams/naLCS.png', 40, null)?>" height="40" /> <span class="hidden-xs hidden-sm">NA LCS</span></h2>
 			<table class="table table-striped">
 				@foreach($nalcs->teams as $team)
 				<tr>
-					<td><a href="/teams/{{ $team->id }}/{{ $team->slug }}"><img src="/img/teams/logos/{{ $team->logo }}" width="18" />&nbsp;&nbsp;<img class="hidden-xs hidden-sm" src="/img/flags/{{ $team->country }}.png" />&nbsp;&nbsp;<span class="hidden-xs hidden-sm">{{ $team->name }}</span></a></td>
+					<td><a href="/teams/{{ $team->id }}/{{ $team->slug }}"><img src="<?=Croppa::url('/img/teams/logos/'.$team->logo, 18, null)?>" width="18" />&nbsp;&nbsp;<img class="hidden-xs hidden-sm" src="/img/flags/{{ $team->country }}.png" />&nbsp;&nbsp;<span class="hidden-xs hidden-sm">{{ $team->name }}</span></a></td>
 				</tr>
 				@endforeach
 			</table>
 		</td>
 		<td valign="top">
-			<h2><img src="/img/teams/ogn.png" height="40" /> <span class="hidden-xs hidden-sm">OGN</span></h2>
+			<h2><img src="<?=Croppa::url('/img/teams/ogn.png', 40, null)?>" height="40" /> <span class="hidden-xs hidden-sm">OGN</span></h2>
 			<table class="table table-striped">
 				@foreach($ogn->teams as $team)
 				<tr>
-					<td><a href="/teams/{{ $team->id }}/{{ $team->slug }}"><img src="/img/teams/logos/{{ $team->logo }}" width="18" />&nbsp;&nbsp;<img class="hidden-xs hidden-sm" src="/img/flags/{{ $team->country }}.png" />&nbsp;&nbsp;<span class="hidden-xs hidden-sm"><span class="hidden-xs hidden-sm">{{ $team->name }}</span></a></td>
+					<td><a href="/teams/{{ $team->id }}/{{ $team->slug }}"><img src="<?=Croppa::url('/img/teams/logos/'.$team->logo, 18, null)?>" width="18" />&nbsp;&nbsp;<img class="hidden-xs hidden-sm" src="/img/flags/{{ $team->country }}.png" />&nbsp;&nbsp;<span class="hidden-xs hidden-sm"><span class="hidden-xs hidden-sm">{{ $team->name }}</span></a></td>
 				</tr>
 				@endforeach
 			</table>
 		</td>
 		<td valign="top">
-			<h2><img src="/img/teams/gpl.png" height="40" /> <span class="hidden-xs hidden-sm">GPL</span></h2>
+			<h2><img src="<?=Croppa::url('/img/teams/gpl.png', 40, null)?>" height="40" /> <span class="hidden-xs hidden-sm">GPL</span></h2>
 			<table class="table table-striped">
 				@foreach($gpl->teams as $team)
 				<tr>
-					<td><a href="/teams/{{ $team->id }}/{{ $team->slug }}"><img src="/img/teams/logos/{{ $team->logo }}" width="18" />&nbsp;&nbsp;<img class="hidden-xs hidden-sm" src="/img/flags/{{ $team->country }}.png" />&nbsp;&nbsp;<span class="hidden-xs hidden-sm">{{ $team->name }}</span></a></td>
+					<td><a href="/teams/{{ $team->id }}/{{ $team->slug }}"><img src="<?=Croppa::url('/img/teams/logos/'.$team->logo, 18, null)?>" width="18" />&nbsp;&nbsp;<img class="hidden-xs hidden-sm" src="/img/flags/{{ $team->country }}.png" />&nbsp;&nbsp;<span class="hidden-xs hidden-sm">{{ $team->name }}</span></a></td>
 				</tr>
 				@endforeach
 			</table>
 		</td>
 		<td valign="top">
-			<h2><img src="/img/teams/lpl.png" height="40" /> <span class="hidden-xs hidden-sm">LPL</span></h2>
+			<h2><img src="<?=Croppa::url('/img/teams/lpl.png', 40, null)?>" height="40" /> <span class="hidden-xs hidden-sm">LPL</span></h2>
 			<table class="table table-striped">
 				@foreach($lpl->teams as $team)
 				<tr>
-					<td><a href="/teams/{{ $team->id }}/{{ $team->slug }}"><img src="/img/teams/logos/{{ $team->logo }}" width="18" />&nbsp;&nbsp;<img class="hidden-xs hidden-sm" src="/img/flags/{{ $team->country }}.png" />&nbsp;&nbsp;<span class="hidden-xs hidden-sm">{{ $team->name }}</span></a></td>
+					<td><a href="/teams/{{ $team->id }}/{{ $team->slug }}"><img src="<?=Croppa::url('/img/teams/logos/'.$team->logo, 18, null)?>" width="18" />&nbsp;&nbsp;<img class="hidden-xs hidden-sm" src="/img/flags/{{ $team->country }}.png" />&nbsp;&nbsp;<span class="hidden-xs hidden-sm">{{ $team->name }}</span></a></td>
 				</tr>
 				@endforeach
 			</table>
