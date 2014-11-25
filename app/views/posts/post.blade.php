77613fb40759ddeb39b7923d9fa8f70b372bcbe7
Mark Tomicki <mark@lolquest.net>
2014-11-25 21:40:25 +0100

    autor to artikle

--- app/views/posts/post.blade.php
+++ app/views/posts/post.blade.php
@@ -8,4 +8,18 @@
 </div>
 -->
 {{ $post->content }}<br/>
+<div class="autor_box">
+	<table>
+		<tr>
+			<td width="100">
+				<a href="/users/{{ $post->user->id }}"><img src="/img/team/{{ $post->user->image }}" class="img-circle" width="75" /></a>
+			</td>
+			<td valign="top">
+				Artikel wurde geschrieben von: <strong><a href="/users/{{ $post->user->id }}">{{ $post->user->first_name }} '{{ $post->user->username }}' {{ $post->user->last_name }}</a></strong><br/>
+				<i>{{ $post->user->description }}</i>
+			</td>
+		</tr>
+	</table>
+</div>
+<br/>
 @include("layouts.disqus")
