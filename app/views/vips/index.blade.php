5f95a63bd2cdad2d9ae399a27a0d1e568182870d
Mark Tomicki <mark@lolquest.net>
2014-11-27 12:00:46 +0100

    headlines to vips

--- app/views/vips/index.blade.php
+++ app/views/vips/index.blade.php
@@ -3,6 +3,8 @@
 @section('subtitle', "Professionelle Kommentaroren und Hosts")
 @section('content')
 	@foreach($vips as $vip)
+		<h2 class="headline">{{$vip->first_name}} '{{$vip->nickname}}' {{$vip->last_name}}</h2>
 		<a href="/vips/{{ $vip->id }}/{{ $vip->slug }}"><img src="/img/vips/{{ $vip->header_image }}" /></a>
+		<br/><br/>
 	@endforeach
 @stop
