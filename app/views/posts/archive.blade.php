20a3ff5003ad4071976590d384cfe43e665a96a9
Mark Tomicki <mark@lolquest.net>
2014-12-09 14:01:07 +0100

    disqus fix

--- app/views/posts/archive.blade.php
+++ app/views/posts/archive.blade.php
@@ -23,7 +23,7 @@
 							<tr class="{{ $class }}">
 								<td width="40"><a href="/news/{{ $post->id }}/{{ $post->slug }}"><img src="<?=Croppa::url('/uploads/news/'.$post->image, 50, null)?>" style="margin-left: 10px;margin-right: 10px;" /></a></td>
 								<td width="90"><span class="meta">{{ Helpers::diffForHumans($post->created_at) }}&nbsp;&nbsp;</span></td>
-								<td width="80"><span class="comments_count"><a href="http://flashigite.com/news/{{ $post->id }}/{{ $post->slug }}#disqus_thread">0 Kommentare</a></span></td>
+								<td width="80"><span class="comments_count"><a class="disqus-comment-count" href="/news/{{ $post->id }}/{{ $post->slug }}#disqus_thread" data-disqus-identifier="/news/{{ $post->id }}/{{ $post->slug }}">0 Kommentare</a></span></td>
 								<td><a class="small_headline" href="/news/{{ $post->id }}/{{ $post->slug }}">{{ $post->title }}</a></td>
 							</tr>
 						</table>
