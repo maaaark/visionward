73e46790f9db5fc19782232d3cf2d9eb752871d5
Benedict Romp <b.romp@meeva.de>
2015-05-11 09:51:09 +0200

    Esports Matches: Design Anpassungen

--- app/views/esports/tournament/game_detail.blade.php
+++ app/views/esports/tournament/game_detail.blade.php
@@ -1,106 +1,112 @@
-@for($i = 100; $i <= 200; $i = $i + 100)
-	<div class="game_element" data-gamecount="{{ $game_count }}">
-		@if($i == 100)
-			<div class="team_name">
-				<img class="team_icon" src="{{ $team1->logo_riot }}">
-				<div class="info">
-					{{ $team1->name }}
-					@if($game->winner > 0)
-						@if($game->winner == $team1->team_id)
-							<div class="winner win">Gewinner dieses Spiels</div>
+@if($game->winner > 0)
+	<div class="game_element" data-gamecount="{{ $game_count }}" id="game_element_{{ $game_count }}">
+	@for($i = 100; $i <= 200; $i = $i + 100)
+			@if($i == 100)
+				<div class="team_name">
+					<img class="team_icon" src="{{ $team1->logo_riot }}">
+					<div class="info">
+						{{ $team1->name }}
+						@if($game->winner > 0)
+							@if($game->winner == $team1->team_id)
+								<div class="winner win">Gewinner dieses Spiels</div>
+							@else
+								<div class="winner loss">Verlierer dieses Spiels</div>
+							@endif
 						@else
-							<div class="winner loss">Verlierer dieses Spiels</div>
+							<div class="winner">Gewinner noch nicht bekannt</div>
 						@endif
-					@else
-						<div class="winner">Gewinner noch nicht bekannt</div>
-					@endif
+					</div>
+					<div style="clear:both;"></div>
 				</div>
-				<div style="clear:both;"></div>
-			</div>
-		@else
-			<div class="team_name">
-				<img class="team_icon" src="{{ $team2->logo_riot }}">
-				<div class="info">
-					{{ $team2->name }}
-					@if($game->winner > 0)
-						@if($game->winner == $team2->team_id)
-							<div class="winner win">Gewinner dieses Spiels</div>
+			@else
+				<div class="team_name">
+					<img class="team_icon" src="{{ $team2->logo_riot }}">
+					<div class="info">
+						{{ $team2->name }}
+						@if($game->winner > 0)
+							@if($game->winner == $team2->team_id)
+								<div class="winner win">Gewinner dieses Spiels</div>
+							@else
+								<div class="winner loss">Verlierer dieses Spiels</div>
+							@endif
 						@else
-							<div class="winner loss">Verlierer dieses Spiels</div>
+							<div class="winner">Gewinner noch nicht bekannt</div>
 						@endif
-					@else
-						<div class="winner">Gewinner noch nicht bekannt</div>
-					@endif
+					</div>
+					<div style="clear:both;"></div>
 				</div>
-				<div style="clear:both;"></div>
-			</div>
-		@endif
+			@endif
 
-		@if(isset($player) AND count($player) > 0)
-			<table class="game_table">
-				<thead>
-					<th>Spieler</th>
-					<th>Champion</th>
-					<th>K/D/A</th>
-					<th>Items</th>
-					<th>Spells</th>
-					<th>Gold</th>
-					<th>Lasthits</th>
-				</thead>
-				<tbody>
-				@foreach($player as $current_player)
-					<?php $show_status = false; ?>
-					@if($i == 100 AND $current_player["teamId"] == $game->blueteam_id)
-						<?php $show_status = true; ?>
-					@elseif($i == 200 AND $current_player["teamId"] == $game->redteam_id)
-						<?php $show_status = true; ?>
-					@endif
+			@if(isset($player) AND count($player) > 0)
+				<table class="game_table">
+					<thead>
+						<th>Spieler</th>
+						<th>Champion</th>
+						<th>K/D/A</th>
+						<th>Items</th>
+						<th>Spells</th>
+						<th>Gold</th>
+						<th>Lasthits</th>
+					</thead>
+					<tbody>
+					@foreach($player as $current_player)
+						<?php $show_status = false; ?>
+						@if($i == 100 AND $current_player["teamId"] == $game->blueteam_id)
+							<?php $show_status = true; ?>
+						@elseif($i == 200 AND $current_player["teamId"] == $game->redteam_id)
+							<?php $show_status = true; ?>
+						@endif
 
-					@if($show_status)
-						<tr class="player_tr">
-							<td class="player">
-								<div style="background-image: url({{ $current_player["photoURL"] }})" class="player_image"></div>
-								<div class="player_name">
-									{{ $current_player["name"] }}
-									<div class="lane_info">Top-Lane</div>
-								</div>
-							</td>
-							<td class="champion">
-								<?php $champ = Helpers::getChampionById($current_player["championId"]); ?>
-								<img class="champion_icon" src="http://ddragon.leagueoflegends.com/cdn/{{ $patchversion }}/img/champion/{{ trim($champ["key"]) }}.png">
-								<div class="level_info">Level: {{ $current_player["endLevel"] }}</div>
-							</td>
-							<td class="kda">
-								{{ $current_player["kills"] }} / {{ $current_player["deaths"] }} / {{ $current_player["assists"] }}
-							</td>
-							<td class="items">
-								<div>
-								@for($item = 0; $item < 6; $item++)
-									@if(isset($current_player["items".$item]) AND $current_player["items".$item] > 0)
-										<img class="item_icon" src="http://ddragon.leagueoflegends.com/cdn/{{ $patchversion }}/img/item/{{ $current_player["items".$item] }}.png">
-									@else
-										<!--<div class="item_icon no_icon"></div>-->
-									@endif
-								@endfor
-								</div>
-							</td>
-							<td class="spells">
-								<img class="spell_icon" src="/img/spells/{{ $current_player["spell0"] }}.png">
-								<img class="spell_icon" src="/img/spells/{{ $current_player["spell1"] }}.png">
-							</td>
-							<td class="gold">
-								{{ number_format($current_player["totalGold"], 0, ",", ".") }}
-							</td>
-							<td class="lasthits">
-								{{ number_format($current_player["minionsKilled"], 0, ",", ".") }}
-							</td>
-						</tr>
-					@endif
-				@endforeach
-				</tbody>
-			</table>
-		@else
+						@if($show_status)
+							<tr class="player_tr">
+								<td class="player">
+									<div style="background-image: url({{ $current_player["photoURL"] }})" class="player_image"></div>
+									<div class="player_name">
+										{{ $current_player["name"] }}
+										<div class="lane_info">Top-Lane</div>
+									</div>
+								</td>
+								<td class="champion">
+									<?php $champ = Helpers::getChampionById($current_player["championId"]); ?>
+									<img class="champion_icon" src="http://ddragon.leagueoflegends.com/cdn/{{ $patchversion }}/img/champion/{{ trim($champ["key"]) }}.png">
+									<div class="level_info">Level: {{ $current_player["endLevel"] }}</div>
+								</td>
+								<td class="kda">
+									{{ $current_player["kills"] }} / {{ $current_player["deaths"] }} / {{ $current_player["assists"] }}
+								</td>
+								<td class="items">
+									<div>
+									@for($item = 0; $item < 6; $item++)
+										@if(isset($current_player["items".$item]) AND $current_player["items".$item] > 0)
+											<img class="item_icon" src="http://ddragon.leagueoflegends.com/cdn/{{ $patchversion }}/img/item/{{ $current_player["items".$item] }}.png">
+										@else
+											<!--<div class="item_icon no_icon"></div>-->
+										@endif
+									@endfor
+									</div>
+								</td>
+								<td class="spells">
+									<img class="spell_icon" src="/img/spells/{{ $current_player["spell0"] }}.png">
+									<img class="spell_icon" src="/img/spells/{{ $current_player["spell1"] }}.png">
+								</td>
+								<td class="gold">
+									{{ number_format($current_player["totalGold"], 0, ",", ".") }}
+								</td>
+								<td class="lasthits">
+									{{ number_format($current_player["minionsKilled"], 0, ",", ".") }}
+								</td>
+							</tr>
+						@endif
+					@endforeach
+					</tbody>
+				</table>
+			@else
 
-		@endif
+			@endif
+		@endfor
 	</div>
-@endfor
+@else
+	<div class="game_not_played_yet">
+		Es sind noch keine weiteren Informationen über dieses Spiel bekannt.
+	</div>
+@endif
--- app/views/esports/tournament/match_detail.blade.php
+++ app/views/esports/tournament/match_detail.blade.php
@@ -10,7 +10,32 @@
 	<script>$(".esports_opener_navi .esports_header_navi .element.matches").addClass("active");</script>
 	<h1>Übersicht</h1>
 	@if(trim($games) != "")
-		{{ $games }}
+		<div class="games_holder" id="games_holder">
+			<div id="games_navi" class="games_navi"></div>
+			<div style="clear: both;"></div>
+			<div id="games_content" class="games_content">{{ $games }}</div>
+		</div>
+
+		<script>
+		var game_count = 1;
+		$("#games_holder #games_content .game_element").each(function(){
+			tab_class = "game_tab";
+			if(game_count == 1){
+				$(this).addClass("open");
+				tab_class= "game_tab open";
+			}
+			$("#games_holder #games_navi").append('<div class="'+tab_class+'" data-gamecount="'+$(this).attr("data-gamecount")+'">Spiel '+game_count+'</div>');
+			game_count++;
+		});
+
+		$("#games_holder #games_navi .game_tab").click(function(){
+			gamecount = $(this).attr("data-gamecount");
+			$("#games_holder #games_navi .open").removeClass("open");
+			$("#games_holder #games_content .open").removeClass("open");
+			$("#games_holder #games_content .game_element#game_element_"+gamecount).addClass("open");
+			$(this).addClass("open");
+		});
+		</script>
 	@else
 		Es sind noch keine Spiel-Informationen vorhanden
 	@endif
