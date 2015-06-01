9f7a487c20086f2d7588e7c4e72915b8756ee15c
Benedict Romp <benedict.romp@googlemail.com>
2015-06-01 22:41:04 +0200

    Navigationfix

--- app/views/layouts/navigation.blade.php
+++ app/views/layouts/navigation.blade.php
@@ -1,27 +1,27 @@
 <div class="visible-xs-* visible-sm-* hidden-md hidden-lg">
 	<ul>
-		<li><a href="/"><img src="/img/flashignite_logo.png" height="35" style="margin-top: -6px;" /> FLASHIGNITE</a></li>
+		<li><a href="http://{{ MAIN_URL }}"><img src="/img/flashignite_logo.png" height="35" style="margin-top: -6px;" /> FLASHIGNITE</a></li>
 		<li>
 			<a href="#"><span class="glyphicon glyphicon-align-justify"></span></a>
 			<ul class="submenu_mobile">
-				<li><a href="/">NEWS</a></li>
+				<li><a href="http://{{ MAIN_URL }}">News</a></li>
 				<li><a href="#">GUIDES&nbsp;&nbsp;<img src="/img/down.png" width="10"></a>
 					<ul class="sub_submenu_mobile">
-						<li><a href="/articles/1/jungle-guide-420">SEASON 5 JUNGLE</a></li>
-						<li><a href="/matches">VIDEOS</a></li>
+						<li><a href="http://{{ MAIN_URL }}/articles/1/jungle-guide-420">SEASON 5 JUNGLE</a></li>
+						<li><a href="http://{{ MAIN_URL }}/matches">VIDEOS</a></li>
 					</ul>
 				</li>
 				<li><a href="#">ESPORTS&nbsp;&nbsp;<img src="/img/down.png" width="10"></a>
 					<ul class="sub_submenu_mobile">
-						<li><a href="/teams">PRO TEAMS</a></li>
+						<li><a href="http://{{ MAIN_URL }}/teams">PRO TEAMS</a></li>
 						<!--<li><a href="/vips">CASTER / HOSTS</a></li>-->
-						<li><a href="/matches">MATCHES</a></li>
-						<li><a href="/transferlist">TRANSFERLISTE</a></li>
+						<li><a href="http://{{ MAIN_URL }}/matches">MATCHES</a></li>
+						<li><a href="http://{{ MAIN_URL }}/transferlist">TRANSFERLISTE</a></li>
 					</ul>
 				</li>
-				<li><a href="/categories/pbe">PBE</a></li>
-				<li><a href="/champions">CHAMPIONS</a></li>
-				<li><a href="/counterpicks">KONTERPICKS</a></li>
+				<li><a href="http://{{ MAIN_URL }}/categories/pbe">PBE</a></li>
+				<li><a href="http://{{ MAIN_URL }}/champions">CHAMPIONS</a></li>
+				<li><a href="http://{{ MAIN_URL }}/counterpicks">KONTERPICKS</a></li>
 			</ul>
 		</li>
 		<li><a href="#"><span class="glyphicon glyphicon-search"></span></a></li>
@@ -30,25 +30,25 @@
 
 	<div class="col-md-8" style="padding-left: 0; margin-left: 0;">
         <ul class="hidden-xs hidden-sm">
-		<li id="nav_logo"><a href="/"><img src="/img/flashignite_logo.png" height="35" style="margin-top: -6px;" /> FLASHIGNITE</a></li>
-		<li {{ Request::is('/') ? ' class="active"' : '' }}{{ Request::is('news/*') ? ' class="active"' : '' }}><a href="/">NEWS</a></li>
+		<li id="nav_logo"><a href="http://{{ MAIN_URL }}/"><img src="/img/flashignite_logo.png" height="35" style="margin-top: -6px;" /> FLASHIGNITE</a></li>
+		<li {{ Request::is('/') ? ' class="active"' : '' }}{{ Request::is('news/*') ? ' class="active"' : '' }}><a href="http://{{ MAIN_URL }}/">NEWS</a></li>
 		<li {{ Request::is('guides') ? ' class="active"' : '' }}><a href="#">GUIDES&nbsp;&nbsp;<img src="/img/down.png" width="10"></a>
 			<ul class="submenu">
-				<li><a href="/articles/1/jungle-guide-420">SEASON 5 JUNGLE</a></li>
-				<li><a href="/categories/videos">VIDEOS</a></li>
+				<li><a href="http://{{ MAIN_URL }}/articles/1/jungle-guide-420">SEASON 5 JUNGLE</a></li>
+				<li><a href="http://{{ MAIN_URL }}/categories/videos">VIDEOS</a></li>
 			</ul>
 		</li>
-		<li {{ Request::is('categories/esports') ? ' class="active"' : '' }}><a href="/categories/esports">ESPORTS&nbsp;&nbsp;<img src="/img/down.png" width="10"></a>
+		<li {{ Request::is('categories/esports') ? ' class="active"' : '' }}><a href="http://{{ MAIN_URL }}/categories/esports">ESPORTS&nbsp;&nbsp;<img src="/img/down.png" width="10"></a>
 			<ul class="submenu">
-				<li><a href="/teams">PRO TEAMS</a></li>
+				<li><a href="http://{{ MAIN_URL }}/teams">PRO TEAMS</a></li>
 				<!--<li><a href="/vips">CASTER / HOSTS</a></li>-->
-				<li><a href="/matches">MATCHES</a></li>
+				<li><a href="http://{{ MAIN_URL }}/matches">MATCHES</a></li>
 				<li style="margin-right: 0px;"><a href="/transferlist">TRANSFERLISTE</a></li>
 			</ul>
 		</li>
-		<li {{ Request::is('categories/pbe') ? ' class="active"' : '' }}><a href="/categories/pbe">PBE</a></li>
-		<li {{ Request::is('champions/*') ? ' class="active"' : '' }} {{ Request::is('champions') ? ' class="active"' : '' }}><a href="/champions">CHAMPIONS</a></li>
-		<li {{ Request::is('counterpicks/*') ? ' class="active"' : '' }} {{ Request::is('counterpicks') ? ' class="active"' : '' }}><a href="/counterpicks">KONTERPICKS</a></li>
+		<li {{ Request::is('categories/pbe') ? ' class="active"' : '' }}><a href="http://{{ MAIN_URL }}/categories/pbe">PBE</a></li>
+		<li {{ Request::is('champions/*') ? ' class="active"' : '' }} {{ Request::is('champions') ? ' class="active"' : '' }}><a href="http://{{ MAIN_URL }}/champions">CHAMPIONS</a></li>
+		<li {{ Request::is('counterpicks/*') ? ' class="active"' : '' }} {{ Request::is('counterpicks') ? ' class="active"' : '' }}><a href="http://{{ MAIN_URL }}/counterpicks">KONTERPICKS</a></li>
 	    </ul>
     </div>
 	<div class="col-md-4">
@@ -73,16 +73,16 @@
                     </li>
                     <!-- <li><a href="/einstellungen">Einstellungen</a></li> -->
                     @if(Auth::user()->hasRole("admin"))
-                        <li><a href="/admin">Admin Panel</a></li>
+                        <li><a href="http://{{ MAIN_URL }}/admin">Admin Panel</a></li>
                     @endif
-                    <li><a href="/logout">Ausloggen</a></li>
+                    <li><a href="http://{{ MAIN_URL }}/logout">Ausloggen</a></li>
                 </ul>
             @else
-                <a href="/login">LOGIN</a>
+                <a href="http://{{ MAIN_URL }}/login">LOGIN</a>
             @endif
         </li>
         @if(!Auth::check())
-            <li><a href="/register">REGISTRIEREN</a></li>
+            <li><a href="http://{{ MAIN_URL }}/register">REGISTRIEREN</a></li>
         @endif
         <li>
 
