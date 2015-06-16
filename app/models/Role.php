30cbd72a620ee25ed3c33fa72f8922edf81d924e
Mark Tomicki <marktomicki@gmail.com>
2015-06-16 09:25:18 +0200

    fix roles

--- app/models/Role.php
+++ app/models/Role.php
@@ -2,7 +2,8 @@
 
 class Role extends \Eloquent {
 	protected $fillable = [];
-	
+    protected $connection = 'mysql2';
+
 	public function users()
     {
         return $this->belongsToMany('User');
